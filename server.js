e695c3dfe4227776372a70082c31a226676a2eec
Jhoss <jhosser@live.com>
2024-04-16 15:11:21 -0500

    change the logic for the proxy

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
+require('dotenv').config()
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+const axios = require('axios');
+
 
 const app = express();
+const PORT = process.env.PORT || 3050;
 
-const apiProxy = createProxyMiddleware({
-  target: 'https://paiwebservices.paiweb.gov.co:8081',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': '/api/interoperabilidad',
-  },
-  logLevel: 'debug',
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hola carevrg!');
+})
+
+app.post('/api/interoperabilidad/GetEPSPersonaMSS', (req, res) => {
+    axios.post('https://paiwebservices.paiweb.gov.co:8081/api/interoperabilidad/GetEPSPersonaMSS', req.body)
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(error => {
+            console.error('Error en la solicitud:', error.response ? error.response.data : error.message);
+            res.status(500).send('Error en el servidor proxy.');
+        });        
 });
 
-app.use('/api', apiProxy);
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Proxy server runing on port: ${PORT}`);
 });
